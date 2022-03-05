9e8c70847a0f6048cd5dcf2410339063094b39a2
Bob Liu <527080292@qq.com>
2022-03-05 17:21:06 +0800

    url

--- MNIST_first.py
+++ MNIST_first.py
@@ -6,25 +6,28 @@ from sklearn.model_selection import cross_val_predict
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score, recall_score, f1_score
-import os
-import urllib
-train_image="http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz"
-train_label="http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz"
-test_image="http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz"
-test_label="http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz"
+from keras.datasets import mnist
 
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+#print(test_images.shape)
 
+x_train=train_images.reshape((60000,784))
+y_train=train_labels
+x_test=test_images.reshape((10000,784))
+y_test=test_labels
 
-x_train, x_test, y_train, y_test = train_test_split(mnist['data'], mnist['target'], random_state=0)
+#x_train, x_test, y_train, y_test = train_test_split(mnist['data'], mnist['target'], random_state=0)
 clf=SVC(kernel ='rbf')
 clf.fit(x_train,y_train)
-pred=cross_val_predict(clf, x_train,y_train, cv=3)
-
-precision = precision_score(y_test,pred)
-recall = recall_score(y_test, pred)
-print('Random Forest Accuracy Score: %f' %accuracy_score(y_test,pred))
-print('Random Forest Precision: %f' %precision)
-print('Random Forest Recall: %f' %recall)
+#pred=cross_val_predict(clf, x_train,y_train, cv=3)
+pred=clf.predict(x_test)
+print("predict size:",pred.shape)
+print("test size:",y_test.shape)
+#precision = precision_score(y_test,pred)
+#recall = recall_score(y_test, pred)
+print('SVM Accuracy Score: %f' %accuracy_score(y_test,pred))
+#print('Random Forest Precision: %f' %precision)
+#print('Random Forest Recall: %f' %recall)
 
 
 
